99c98c094615e70c55120364e4640ee79c84687c
tekno0ryder <ahmad1734king@gmail.com>
2017-04-30 16:40:49 +0300

    typical Updates

---
+++ ShallalProject/src/UI/LoginController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ryder
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private TextField userName;
+    @FXML
+    private PasswordField password;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onLoginClicked(ActionEvent event) {
+    }
+    
+}
